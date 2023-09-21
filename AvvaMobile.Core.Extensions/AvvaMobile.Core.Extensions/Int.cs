bd0dbbf970dbb0ff905d2d6b1b642afa99781f21
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2023-09-21 18:55:43 +0300

    Math extensions added.

--- AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/Int.cs
+++ AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/Int.cs
@@ -1,64 +1,68 @@
-﻿namespace AvvaMobile.Core.Extensions
+﻿namespace AvvaMobile.Core.Extensions;
+
+public static class Int
 {
-    public static class Int
+    public static string ToTL(this int? val)
     {
-        public static string ToTL(this int? val)
+        if (val == null)
         {
-            if (val == null)
-            {
-                return "0 TL.";
-            }
-            return val.Value.ToString("N0") + " TL.";
+            return "0 TL.";
         }
+        return val.Value.ToString("N0") + " TL.";
+    }
 
-        public static string ToTL(this int val)
-        {
-            return val.ToString("N0") + " TL.";
-        }
+    public static string ToTL(this int val)
+    {
+        return val.ToString("N0") + " TL.";
+    }
 
-        public static bool IsALLSelected(this int val)
-        {
-            return val.Equals(0);
-        }
+    public static bool IsALLSelected(this int val)
+    {
+        return val.Equals(0);
+    }
 
-        /// <summary>
-        /// Returns 0 if object is null, otherwise returns value of the object.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static int DefaultIfEmpty(this int? val)
-        {
-            return val ?? 0;
-        }
+    /// <summary>
+    /// Returns 0 if object is null, otherwise returns value of the object.
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static int DefaultIfEmpty(this int? val)
+    {
+        return val ?? 0;
+    }
 
-        /// <summary>
-        /// Formats the value as N0.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static string ToFormattedPrice(this int val)
-        {
-            return val.ToString("N0");
-        }
+    /// <summary>
+    /// Formats the value as N0.
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static string ToFormattedPrice(this int val)
+    {
+        return val.ToString("N0");
+    }
 
-        /// <summary>
-        /// Formats the value as N0.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static string ToFormattedPrice(this int? val)
-        {
-            return val != null ? val.Value.ToString("N0") : "-";
-        }
+    /// <summary>
+    /// Formats the value as N0.
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static string ToFormattedPrice(this int? val)
+    {
+        return val != null ? val.Value.ToString("N0") : "-";
+    }
 
-        /// <summary>
-        /// Returns an hyphen as empty string indicator if object is null, otherwise returns value of the object.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static string ToEmptyStringIndicator(this int? val)
-        {
-            return val.HasValue ? val.Value.ToString() : "--";
-        }
+    /// <summary>
+    /// Returns an hyphen as empty string indicator if object is null, otherwise returns value of the object.
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static string ToEmptyStringIndicator(this int? val)
+    {
+        return val.HasValue ? val.Value.ToString() : "--";
+    }
+    
+    public static decimal DivideSafely(this decimal Numerator, decimal Denominator)
+    {
+        return (Denominator == 0) ? 0 : Numerator / Denominator;
     }
 }
