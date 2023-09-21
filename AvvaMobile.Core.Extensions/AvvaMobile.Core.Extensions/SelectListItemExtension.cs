fb6edbea29806cb8b0d54b615cc569e3be472899
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2023-09-21 15:31:36 +0300

    SelectListItem extensions updated.

--- AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/SelectListItemExtension.cs
+++ AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/SelectListItemExtension.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace AvvaMobile.Core.Extensions
-{
-    public static class SelectListItemExtension
-    {
-        public static List<SelectListItem> AddALLOption(this List<SelectListItem> list, string text)
-        {
-            list.Insert(0, new SelectListItem { Text = text, Value = "-1" });
-            return list;
-        }
+namespace AvvaMobile.Core.Extensions;
 
-        public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list, string text)
-        {
-            list.Insert(0, new SelectListItem { Text = text, Value = string.Empty });
-            return list;
-        }
+public static class SelectListItemExtension
+{
+    public static List<SelectListItem> AddALLOption(this List<SelectListItem> list, string text)
+    {
+        list.Insert(0, new SelectListItem { Text = text, Value = "-1" });
+        return list;
+    }
+
+    public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list, string text)
+    {
+        list.Insert(0, new SelectListItem { Text = text, Value = string.Empty });
+        return list;
+    }
+        
+    public static List<SelectListItem> AddALLOption(this List<SelectListItem> list)
+    {
+        list.Insert(0, new SelectListItem { Text = "-- Tümü --", Value = "-1" });
+        return list;
+    }
+
+    public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list)
+    {
+        list.Insert(0, new SelectListItem { Text = "-- Seçiniz --", Value = string.Empty });
+        return list;
     }
 }
