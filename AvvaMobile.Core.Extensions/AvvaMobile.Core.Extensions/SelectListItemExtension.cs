d9776453662977331bff7163c66d178e3e19483e
Öcal Esmer <ocal@avvamobile.com>
2024-03-21 17:54:12 +0300

    1.0.13 SelectListItemExtention dışardan parametre alması sağlandı.

--- AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/SelectListItemExtension.cs
+++ AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/SelectListItemExtension.cs
@@ -4,26 +4,22 @@ namespace AvvaMobile.Core.Extensions;
 
 public static class SelectListItemExtension
 {
-    [Obsolete("This method is obsolete. Use AddALLOption without parameter instead.")]
     public static List<SelectListItem> AddALLOption(this List<SelectListItem> list, string text)
     {
-        list.Insert(0, new SelectListItem { Text = text, Value = "-1" });
+        list.Insert(0, new SelectListItem { Text = text, Value = "0" });
         return list;
-    }
-
-    [Obsolete("This method is obsolete. Use AddALLOption without parameter instead.")]
-    public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list, string text)
-    {
-        list.Insert(0, new SelectListItem { Text = text, Value = string.Empty });
-        return list;
-    }
-        
+    }        
     public static List<SelectListItem> AddALLOption(this List<SelectListItem> list)
     {
         list.Insert(0, new SelectListItem { Text = "-- Tümü --", Value = "0" });
         return list;
     }
-
+   
+    public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list, string text)
+    {
+        list.Insert(0, new SelectListItem { Text = text, Value = "-1" });
+        return list;
+    }
     public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list)
     {
         list.Insert(0, new SelectListItem { Text = "-- Seçiniz --", Value = "-1" });
