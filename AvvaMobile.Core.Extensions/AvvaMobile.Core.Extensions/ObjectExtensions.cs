6d7905520ce75fbd3ca9be04768a50e9f31f2798
Yusuf Özel <yusufozell97@gmail.com>
2024-05-02 10:39:54 +0300

    Object Copy eklendi

---
+++ AvvaMobile.Core.Extensions/AvvaMobile.Core.Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AvvaMobile.Core.Extensions;
+
+public static class ObjectExtensions
+{
+    public static T Copy<T>(this T obj)
+    {
+        var json = JsonSerializer.Serialize(obj);
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
